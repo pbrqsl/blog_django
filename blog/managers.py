565e528148d3cb2bb2ae2c3cbb38c305b57a7e32
piotr bronikowski <pbrqsl@gmail.com>
2023-10-22 18:28:40 +0200

    first commit

---
+++ blog/managers.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.db.models.query import QuerySet
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self) -> QuerySet:
+        from blog.models import Post
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+    
+# class PublishedManager(models.Manager):
+#     def get_queryset(self) -> QuerySet:
+#         from blog.models import Post
+#         return super().get_queryset().filter(status='blog.models.Post.Status.PUBLISHED')
+    
